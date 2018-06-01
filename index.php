1a03ec6bf1346c26f5f5567a141fb7abd474e044
Codemis <johnathan@jpulos.com>
2018-06-01 15:49:41 -0700

    Added an initial index file.

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Track Scripture Exporter</title>
+    </head>
+    <body>
+        <h1>Track Scripture Exporter</h1>
+    </body>
+</html>
