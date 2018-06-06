19109b81a76cd6680975b16669f9151f920dbd4c
Codemis <johnathan@jpulos.com>
2018-06-05 17:22:49 -0700

    Added reading the booklets from YAML.

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
+use Symfony\Component\Yaml\Yaml;
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+/**
+ * Get all the booklets from the YAML file
+ */
+$bookletsXml = 'booklets.yaml';
+$booklets = [];
+if (file_exists($bookletsXml)) {
+    $data = Yaml::parseFile('booklets.yaml');
+    $booklets = $data['booklets'];
+}
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -8,5 +20,13 @@
     </head>
     <body>
         <h1>Track Scripture Exporter</h1>
+        <form action="/" name="track-scripture-select">
+            <label for="desired-booklet">Booklet</label><br>
+            <?php
+                foreach ($booklets as $booklet) {
+                    echo '<input type="radio" name="desired-booklet" value="' . $booklet['title'] . '"> <strong>' . $booklet['title'] . '</strong>: ' . $booklet['description'] . '<br>';
+                }
+            ?>
+        </form>
     </body>
 </html>
